d9976787d795c8b82286c6bee04b4e2fdf3dd6cf
Vishnu V O <vishnu.vo@alliance.edu.in>
2024-01-12 12:22:46 +0530

    Updates

---
+++ static/js/my-chunk-file.b8375a16.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_route=self.webpackChunkreact_route||[]).push([[205],{826:(e,r,t)=>{t.r(r),t.d(r,{default:()=>c});t(791);var u=t(184);function c(){return(0,u.jsx)("div",{children:"Section"})}}}]);
+//# sourceMappingURL=my-chunk-file.b8375a16.chunk.js.map
